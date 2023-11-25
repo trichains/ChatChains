03a6f071eaf80f4235bda95780e61b6bd9286614
Cristhian <trichains@hotmail.com>
2023-11-25 19:30:46 -0400

    add test

--- assets/main.js
+++ assets/main.js
@@ -1,66 +1,61 @@
-const apiKey = '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';
+const apiKey = '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';
 
-// Cache DOM elements
-const msg = document.getElementById('msg-input');
-const statusMsg = document.getElementById('status');
-const btnSubmit = document.getElementById('btn-submit');
-const historico = document.getElementById('historico');
+function sendMsg() {
+  var msg = document.getElementById('msg-input');
+  if (!msg.value) {
+    msg.style.border = '1px solid red';
+    return;
+  }
+  msg.style.border = 'none';
 
-// Event delegation for submit button
-document.addEventListener('click', async (event) => {
-  if (event.target.matches('#btn-submit')) {
-    event.preventDefault();
+  var status = document.getElementById('status');
+  var btnSubmit = document.getElementById('btn-submit');
 
-    if (!msg.value) {
-      msg.style.border = '1px solid red';
-      return;
-    }
-    msg.style.border = 'none';
+  status.style.display = 'block';
+  status.innerHTML = 'Gerando resposta...';
+  btnSubmit.disabled = true;
+  btnSubmit.style.cursor = 'not-allowed';
+  msg.disabled = true;
 
-    statusMsg.style.display = 'block';
-    statusMsg.innerHTML = 'Gerando resposta...';
-    btnSubmit.disabled = true;
-    btnSubmit.style.cursor = 'not-allowed';
-    msg.disabled = true;
-
-    try {
-      const response = await fetch('https://api.openai.com/v1/completions', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${apiKey}`
-        },
-        body: JSON.stringify({
-          model: 'gpt-3.5-turbo-instruct',
-          prompt: msg.value,
-          max_tokens: 1000,
-          temperature: 0.5
-        })
-      });
-
-      const data = await response.json();
-      const r = data.choices[0].text;
-
-      statusMsg.style.display = 'none';
+  fetch('https://api.openai.com/v1/completions', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      model: 'text-davinci-003',
+      prompt: msg.value,
+      max_tokens: 2048,
+      temperature: 0.5
+    })
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      status.style.display = 'none';
       showHistorico(msg.value, r);
-    } catch (error) {
+    })
+    .catch((error) => {
       console.log(error);
-    } finally {
-      statusMsg.innerHTML = '';
+    })
+    .finally(() => {
+      status.innerHTML = '';
       btnSubmit.disabled = false;
       btnSubmit.style.cursor = 'pointer';
       msg.disabled = false;
-    }
-  }
-});
+    });
+}
 
 function showHistorico(msg, response) {
+  var historico = document.getElementById('historico');
+
   // minhas mensagens
-  const boxMyMsg = document.createElement('div');
+  var boxMyMsg = document.createElement('div');
   boxMyMsg.className = 'box-my-msg';
 
-  const myMsg = document.createElement('p');
+  var myMsg = document.createElement('p');
   myMsg.className = 'my-msg';
   myMsg.innerHTML = msg;
 
@@ -68,10 +63,10 @@ function showHistorico(msg, response) {
   historico.appendChild(boxMyMsg);
 
   // mensagens de retorno
-  const boxResponseMsg = document.createElement('div');
+  var boxResponseMsg = document.createElement('div');
   boxResponseMsg.className = 'box-response-msg';
 
-  const chatResponse = document.createElement('p');
+  var chatResponse = document.createElement('p');
   chatResponse.className = 'chat-msg';
   chatResponse.innerHTML = response;
 
