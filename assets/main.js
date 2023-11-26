f9ab68741b3377a3b6a5fc25a2674af2fb71f6ee
Cristhian <trichains@hotmail.com>
2023-11-26 18:43:31 -0400

    att apiKey e actions secrets

--- assets/main.js
+++ assets/main.js
@@ -19,7 +19,7 @@ function sendMsg() {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+      Authorization: `Bearer ${REACT_APP_API_KEY}`
     },
     body: JSON.stringify({
       model: 'gpt-3.5-turbo-instruct',
