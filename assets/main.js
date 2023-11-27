3a590f9b62c05529c953b93f4101d108da24aad7
Cristhian <trichains@hotmail.com>
2023-11-27 00:45:29 -0400

    add: chat de saida do usuario

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,31 @@
-console.log('test');
+const chatInput = document.getElementById('chat-input');
+const sendBtn = document.getElementById('send-btn');
+const chatContainer = document.querySelector('.chat-container');
 
+let userText = null;
 
+const createElement = (html, className) => {
+  // Cria um elemento HTML com base no HTML passado como parâmetro e o nome da classe passado como parâmetro
+  const chatDiv = document.createElement('div');
+  chatDiv.classList.add('chat', className);
+  chatDiv.innerHTML = html;
+  return chatDiv; // retorna o elemento
+};
 
+const handleSaidaChat = () => {
+  userText = chatInput.value.trim(); // retorna o valor e remove espaços em branco
+  const html = `<div class="chat content">
+                  <div class="chat-details">
+                    <img src="./assets/imgs/user.svg" alt="Foto do usuário" />
+                    <p>${userText}</p>
+                 </div>
+                </div>`;
+  // crie um div de chat de saída com a mensagem do usuário e anexe-o ao contêiner de chat
+  const saidaChatDiv = createElement(html, 'saida');
+  chatContainer.appendChild(saidaChatDiv);
+};
 
+sendBtn.addEventListener('click', handleSaidaChat);
 
 // fetch('https://api.openai.com/v1/chat/completions', {
 //     method: 'POST',
