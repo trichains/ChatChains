3c61674833cc216e0dcfec5ff7927ab9b5539525
Cristhian <trichains@hotmail.com>
2023-11-27 16:33:46 -0400

    documentando main.js

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,24 @@
 // Seleção de Elementos do DOM
 const elements = {
-  chatInput: document.getElementById('chat-input'),
-  sendBtn: document.getElementById('send-btn'),
-  chatContainer: document.querySelector('.chat-container'),
-  themeBtn: document.getElementById('theme-btn'),
-  githubIcon: document.querySelector('.rede-social img'),
-  deleteBtn: document.getElementById('delete-btn')
+  chatInput: document.getElementById('chat-input'), // Input de texto do usuário
+  sendBtn: document.getElementById('send-btn'), // Botão de envio de mensagem
+  chatContainer: document.querySelector('.chat-container'), // Contêiner para exibição das mensagens do chat
+  themeBtn: document.getElementById('theme-btn'), // Botão para mudar o tema
+  githubIcon: document.querySelector('.rede-social img'), // Seleção do elemento do ícone do GitHub
+  deleteBtn: document.getElementById('delete-btn') // Botão para apagar o chat
 };
 
 // Variáveis Globais
-let userText = null;
-const apiUrl = 'https://botchains.vercel.app/api/openai';
-const initialHeight = elements.chatInput.scrollHeight;
+let userText = null; // Armazena o texto digitado pelo usuário
+const apiUrl = 'https://botchains.vercel.app/api/openai'; // URL da API OpenAI
+const initialHeight = elements.chatInput.scrollHeight; // Altura inicial do input
 
+// Carrega dados do localStorage ao iniciar
 const loadDataFromLocalStorage = () => {
   const themeColor = localStorage.getItem('theme-color');
+  // Adiciona ou remove a classe 'light-mode' com base na preferência do tema
   document.body.classList.toggle('light-mode', themeColor === 'light_mode');
+  // Atualiza o texto do botão de tema com base no tema atual
   elements.themeBtn.textContent = document.body.classList.contains('light-mode')
     ? 'dark_mode'
     : 'light_mode';
@@ -25,11 +28,13 @@ const loadDataFromLocalStorage = () => {
                           <p> Comece a conversar e explore o poder da AI.<br> O histórico do seu chat aparecerá aqui.<br>
                           Desenvolvido por <a href='https://github.com/trichains' target='_blank'>trichains</a></p>
                         </div>`;
+  // Carrega o histórico do chat do localStorage ou exibe um texto padrão
   elements.chatContainer.innerHTML =
     localStorage.getItem('all-chats') || defaultText;
   elements.chatContainer.scrollTo(0, elements.chatContainer.scrollHeight);
 };
 
+// Chama a função para carregar dados do localStorage ao iniciar
 loadDataFromLocalStorage();
 
 // Função de Criação de Elemento HTML
@@ -37,7 +42,7 @@ const createElement = (html, className) => {
   const chatDiv = document.createElement('div');
   chatDiv.classList.add('chat', className);
   chatDiv.innerHTML = html;
-  return chatDiv;
+  return chatDiv; // Retorna o elemento criado
 };
 
 // Função para Obter Resposta do Chat
@@ -59,15 +64,18 @@ const getChatResponse = (entradaChatDiv, response) => {
     pElement.textContent = 'Resposta inválida da API';
   }
 
+  // Remove a animação de digitação, adiciona o elemento p e salva o conteúdo do chat no localStorage
   entradaChatDiv.querySelector('.typing-animation').remove();
   entradaChatDiv.querySelector('.chat-details').appendChild(pElement);
   elements.chatContainer.scrollTo(0, elements.chatContainer.scrollHeight);
   localStorage.setItem('all-chats', elements.chatContainer.innerHTML);
 };
 
+// Função para copiar resposta para a área de transferência
 const copyResponse = (copyBtn) => {
   const responseTextElement = copyBtn.parentElement.querySelector('p');
   navigator.clipboard.writeText(responseTextElement.textContent);
+  // Restaura o texto do botão após a cópia
   setTimeout(() => {
     copyBtn.textContent = 'content_copy';
   }, 2000);
@@ -102,9 +110,11 @@ const showTypingAnimation = async () => {
     }
 
     const responseData = await response.json();
+    // Chama a função para manipular a resposta
     getChatResponse(entradaChatDiv, responseData);
   } catch (error) {
     console.error('Erro ao obter resposta da API OpenAI', error);
+    // Caso ocorra um erro, você pode manipulá-lo aqui, se necessário
   }
 };
 
@@ -121,11 +131,14 @@ const handleSaidaChat = () => {
                     <p></p>
                  </div>
                 </div>`;
+  // Cria um div de chat de saída com a mensagem do usuário e anexa ao contêiner de chat
   const saidaChatDiv = createElement(html, 'saida');
   saidaChatDiv.querySelector('.chat-details p').textContent = userText;
   document.querySelector('.default-text')?.remove();
   elements.chatContainer.appendChild(saidaChatDiv);
   elements.chatContainer.scrollTo(0, elements.chatContainer.scrollHeight);
+
+  // Após criar a entrada do usuário, você pode chamar a função para obter a resposta (showTypingAnimation)
   showTypingAnimation();
 };
 
@@ -133,22 +146,27 @@ const handleSaidaChat = () => {
 const toggleGithubIcon = () => {
   const isLightMode = document.body.classList.contains('light-mode');
   const iconPath = isLightMode
-    ? './assets/imgs/github-dark.svg'
-    : './assets/imgs/github.svg';
+    ? './assets/imgs/github-dark.svg' // Caminho para o ícone escuro
+    : './assets/imgs/github.svg'; // Caminho para o ícone claro
   elements.githubIcon.setAttribute('src', iconPath);
 };
 
 // Adiciona um ouvinte de evento para o clique no botão de tema
 elements.themeBtn.addEventListener('click', () => {
+  // Muda o tema do site
   document.body.classList.toggle('light-mode');
   localStorage.setItem('theme-color', elements.themeBtn.textContent);
   elements.themeBtn.textContent = document.body.classList.contains('light-mode')
     ? 'dark_mode'
     : 'light_mode';
+
+  // Chama a função para trocar o ícone do GitHub
   toggleGithubIcon();
 });
 
+// Adiciona um ouvinte de evento para o clique no botão de apagar
 elements.deleteBtn.addEventListener('click', () => {
+  // Remove todas as conversas do localStorage e chama a função loadDataFromLocalStorage para atualizar o conteúdo do chat
   if (confirm('Deseja apagar todo o histórico da conversa?')) {
     localStorage.removeItem('all-chats');
     elements.chatContainer.innerHTML = '';
@@ -156,16 +174,21 @@ elements.deleteBtn.addEventListener('click', () => {
   }
 });
 
+// Adiciona um ouvinte de evento para o evento de entrada no campo de texto
 elements.chatInput.addEventListener('input', () => {
+  // Ajusta a altura do input de acordo com o conteúdo
   elements.chatInput.style.height = `${initialHeight}px`;
   elements.chatInput.style.height = `${elements.chatInput.scrollHeight}px`;
 });
 
+// Adiciona um ouvinte de evento para o pressionamento de tecla no campo de texto
 elements.chatInput.addEventListener('keydown', (e) => {
+  // Se o botão Enter for pressionado com shift pressionado e a largura da janela for maior que 768, aciona a manipulação da saída do chat
   if (e.key === 'Enter' && !e.shiftKey && window.innerWidth > 768) {
     e.preventDefault();
     handleSaidaChat();
   }
 });
 
+// Adiciona um ouvinte de evento para o clique no botão de envio, que aciona a manipulação da saída do chat
 elements.sendBtn.addEventListener('click', handleSaidaChat);
