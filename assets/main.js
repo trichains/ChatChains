ed719d8c54ac708e264cc567bdc75dedf795fd8c
Cristhian <trichains@hotmail.com>
2023-11-28 00:52:45 -0400

    att

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
 // Seleção de Elementos do DOM
-const elements = {
+const domElements = {
   chatInput: document.getElementById('chat-input'),
   sendBtn: document.getElementById('send-btn'),
   chatContainer: document.querySelector('.chat-container'),
@@ -10,7 +10,7 @@ const elements = {
 
 // Constantes
 const apiUrl = 'https://botchains.vercel.app/api/openai';
-const initialHeight = elements.chatInput.scrollHeight;
+const initialHeight = domElements.chatInput.scrollHeight;
 
 // Carrega dados do localStorage ao iniciar
 const loadLocalStorageData = () => {
@@ -18,7 +18,7 @@ const loadLocalStorageData = () => {
   const isLightMode = themeColor === 'light_mode';
 
   document.body.classList.toggle('light-mode', isLightMode);
-  elements.themeBtn.textContent = isLightMode ? 'dark_mode' : 'light_mode';
+  domElements.themeBtn.textContent = isLightMode ? 'dark_mode' : 'light_mode';
 
   const defaultText = `<div class='default-text'>
                           <h1>⛓️ Bot<span class='destaque'>Chains</span> ⛓️</h1>
@@ -26,9 +26,9 @@ const loadLocalStorageData = () => {
                           Desenvolvido por <a href='https://github.com/trichains' target='_blank'>trichains</a></p>
                         </div>`;
 
-  elements.chatContainer.innerHTML =
+  domElements.chatContainer.innerHTML =
     localStorage.getItem('all-chats') || defaultText;
-  elements.chatContainer.scrollTo(0, elements.chatContainer.scrollHeight);
+  domElements.chatContainer.scrollTo(0, domElements.chatContainer.scrollHeight);
 };
 
 // Chama a função para carregar dados do localStorage ao iniciar
@@ -69,8 +69,8 @@ const handleChatResponse = (chatEntry, response) => {
 
   chatEntry.querySelector('.typing-animation').remove();
   chatEntry.querySelector('.chat-details').appendChild(pElement);
-  elements.chatContainer.scrollTo(0, elements.chatContainer.scrollHeight);
-  localStorage.setItem('all-chats', elements.chatContainer.innerHTML);
+  domElements.chatContainer.scrollTo(0, domElements.chatContainer.scrollHeight);
+  localStorage.setItem('all-chats', domElements.chatContainer.innerHTML);
 };
 
 // Função para copiar resposta para a área de transferência
@@ -97,14 +97,14 @@ const showTypingAnimation = async () => {
                   <span onclick='copyResponse(this)' class='material-symbols-rounded'>content_copy</span>
                 </div>`;
   const chatEntry = createElement(html, 'entrada');
-  elements.chatContainer.appendChild(chatEntry);
-  elements.chatContainer.scrollTo(0, elements.chatContainer.scrollHeight);
+  domElements.chatContainer.appendChild(chatEntry);
+  domElements.chatContainer.scrollTo(0, domElements.chatContainer.scrollHeight);
 
   try {
     const response = await fetch(apiUrl, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ userText: elements.chatInput.value.trim() })
+      body: JSON.stringify({ userText })
     });
 
     if (!response.ok) {
@@ -130,39 +130,30 @@ const showError = (errorMessage) => {
                  </div>
                 </div>`;
   const errorChatEntry = createElement(html, 'saida');
-  elements.chatContainer.appendChild(errorChatEntry);
-  elements.chatContainer.scrollTo(0, elements.chatContainer.scrollHeight);
-  localStorage.setItem('all-chats', elements.chatContainer.innerHTML);
+  domElements.chatContainer.appendChild(errorChatEntry);
+  domElements.chatContainer.scrollTo(0, domElements.chatContainer.scrollHeight);
+  localStorage.setItem('all-chats', domElements.chatContainer.innerHTML);
 };
-
-document.addEventListener('DOMContentLoaded', function () {
-  var jsDisabledMessage = document.querySelector('.js-disable');
-  jsDisabledMessage.style.display = 'block'; // Exibe a mensagem inicialmente
-
-  // Adiciona a classe js-enabled ao body para estilizar elementos quando o JavaScript está habilitado
-  document.body.classList.add('js-enabled');
-
-  // Remove a mensagem se o JavaScript estiver habilitado
-  jsDisabledMessage.style.display = 'none';
-});
 
 // Manipulação da Saída do Chat
 const handleChatOutput = () => {
-  const userText = elements.chatInput.value.trim();
+  userText = domElements.chatInput.value.trim();
   if (!userText) return;
 
-  elements.chatInput.value = '';
-  elements.chatInput.style.height = `${initialHeight}px`;
+  domElements.chatInput.value = '';
+  domElements.chatInput.style.height = `${initialHeight}px`;
   const html = `<div class="chat-content">
                   <div class="chat-details">
                     <img src="./assets/imgs/user.svg" alt="Foto do usuário" />
-                    <p>${userText}</p>
+                    <p></p>
                  </div>
                 </div>`;
   // Cria um div de chat de saída com a mensagem do usuário e anexa ao contêiner de chat
   const outputChatEntry = createElement(html, 'saida');
-  elements.chatContainer.appendChild(outputChatEntry);
-  elements.chatContainer.scrollTo(0, elements.chatContainer.scrollHeight);
+  outputChatEntry.querySelector('.chat-details p').textContent = userText;
+  document.querySelector('.default-text')?.remove();
+  domElements.chatContainer.appendChild(outputChatEntry);
+  domElements.chatContainer.scrollTo(0, domElements.chatContainer.scrollHeight);
 
   // Após criar a entrada do usuário, você pode chamar a função para obter a resposta (showTypingAnimation)
   showTypingAnimation();
@@ -174,15 +165,17 @@ const toggleGithubIcon = () => {
   const iconPath = isLightMode
     ? './assets/imgs/github-dark.svg' // Caminho para o ícone escuro
     : './assets/imgs/github.svg'; // Caminho para o ícone claro
-  elements.githubIcon.setAttribute('src', iconPath);
+  domElements.githubIcon.setAttribute('src', iconPath);
 };
 
 // Adiciona um ouvinte de evento para o clique no botão de tema
-elements.themeBtn.addEventListener('click', () => {
+domElements.themeBtn.addEventListener('click', () => {
   // Muda o tema do site
   document.body.classList.toggle('light-mode');
-  localStorage.setItem('theme-color', elements.themeBtn.textContent);
-  elements.themeBtn.textContent = document.body.classList.contains('light-mode')
+  localStorage.setItem('theme-color', domElements.themeBtn.textContent);
+  domElements.themeBtn.textContent = document.body.classList.contains(
+    'light-mode'
+  )
     ? 'dark_mode'
     : 'light_mode';
 
@@ -191,24 +184,24 @@ elements.themeBtn.addEventListener('click', () => {
 });
 
 // Adiciona um ouvinte de evento para o clique no botão de apagar
-elements.deleteBtn.addEventListener('click', () => {
+domElements.deleteBtn.addEventListener('click', () => {
   // Remove todas as conversas do localStorage e chama a função loadLocalStorageData para atualizar o conteúdo do chat
   if (confirm('Deseja apagar todo o histórico da conversa?')) {
     localStorage.removeItem('all-chats');
-    elements.chatContainer.innerHTML = '';
+    domElements.chatContainer.innerHTML = '';
     loadLocalStorageData();
   }
 });
 
 // Adiciona um ouvinte de evento para o evento de entrada no campo de texto
-elements.chatInput.addEventListener('input', () => {
+domElements.chatInput.addEventListener('input', () => {
   // Ajusta a altura do input de acordo com o conteúdo
-  elements.chatInput.style.height = `${initialHeight}px`;
-  elements.chatInput.style.height = `${elements.chatInput.scrollHeight}px`;
+  domElements.chatInput.style.height = `${initialHeight}px`;
+  domElements.chatInput.style.height = `${domElements.chatInput.scrollHeight}px`;
 });
 
 // Adiciona um ouvinte de evento para o pressionamento de tecla no campo de texto
-elements.chatInput.addEventListener('keydown', (e) => {
+domElements.chatInput.addEventListener('keydown', (e) => {
   // Se o botão Enter for pressionado com shift pressionado e a largura da janela for maior que 768, aciona a manipulação da saída do chat
   if (e.key === 'Enter' && !e.shiftKey && window.innerWidth > 768) {
     e.preventDefault();
@@ -217,6 +210,6 @@ elements.chatInput.addEventListener('keydown', (e) => {
 });
 
 // Adiciona um ouvinte de evento para o clique no botão de envio, que aciona a manipulação da saída do chat
-elements.sendBtn.addEventListener('click', handleChatOutput);
+domElements.sendBtn.addEventListener('click', handleChatOutput);
 
 toggleGithubIcon();
