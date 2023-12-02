684b55962614cb678c2f6a6acb73d136747793f0
Cristhian <trichains@hotmail.com>
2023-12-02 04:29:53 -0400

    Update openai.js

--- api/openai.js
+++ api/openai.js
@@ -32,8 +32,7 @@ export default async function openaiHandler(req, res) {
       body: JSON.stringify({
         model: openaiModel,
         messages: [{ role: 'user', content: userText }],
-        max_tokens: 4096,
-        temperature: 0.5,
+        language: 'pt',
       })
     };
 
