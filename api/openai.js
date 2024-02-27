3b329b184c13acfd424ff1e28d95e47189c57044
Cristhian Almeida <3correntes@gmail.com>
2024-02-27 12:48:41 -0400

    Update openai.js

--- api/openai.js
+++ api/openai.js
@@ -10,15 +10,18 @@ export default async function openaiHandler(req, res) {
     if (!apiKey) {
       throw new Error('Chave de API não fornecida');
     }
-    // URL da API OpenAI para completar as interações do chat
-    const OPENAI_API_URL = 'https://api.openai.com/v1/chat/completions';
+
+    const openaiModel = process.env.OPENAI_MODEL;
+
+    // Define a URL da API OpenAI
+    const API_URL = 'https://api.openai.com/v1/chat/completions';
+
     // Obtenha os dados da requisição do cliente
     const { userText } = req.body;
     // Certifique-se de que o texto do usuário está presente na requisição
     if (!userText) {
       throw new Error('Texto do usuário não fornecido');
     }
-    const openaiModel = process.env.OPENAI_MODEL;
 
     // Adiciona a mensagem do usuário ao histórico
     conversationHistory.push({ role: 'user', content: userText });
@@ -32,31 +35,18 @@ export default async function openaiHandler(req, res) {
       },
       body: JSON.stringify({
         model: openaiModel,
-        messages: [
-          {
-            role: 'system',
-            content:
-              'Eu sou o ChatChains, um assistente virtual desenvolvido para fornecer informações e assistência e fui desenvolvido por trichains.'
-          },
-
-          {
-            role: 'user',
-            content: 'Posso obter informações em português?'
-          }, // Adicione uma mensagem em português
-          {
-            role: 'assistant',
-            content: userText
-          },
-          ...conversationHistory // Adiciona o histórico da conversa
-        ]
+        messages: conversationHistory
       })
     };
+
     // Faz a chamada à API OpenAI usando o módulo 'fetch'
-    const response = await fetch(OPENAI_API_URL, requestOptions);
+    const response = await fetch(API_URL, requestOptions);
+
     // Verifica se a chamada à API foi bem-sucedida
     if (!response.ok) {
       throw new Error(`OpenAI API error: ${response.statusText}`);
     }
+
     // Obtém os dados da resposta da API
     const responseData = await response.json();
 
